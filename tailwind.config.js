e3f8035c5a9322f9b68bdf57334861bac5963538
Jared Dantis <jareddantis@gmail.com>
2022-06-14 17:13:49 +0800

    Create Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./static/scripts/**/*.js",
+    "./static/styles/**/*.css",
+    "./templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
